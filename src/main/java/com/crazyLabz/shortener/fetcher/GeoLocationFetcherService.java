6c3843e02aa40566d33fe6afbb8503f0c70aa723
amaziagur <amazia@payayaglobal.com>
2019-02-10 15:06:38 +0200

    fix change in geo location vendor

--- src/main/java/com/crazyLabz/shortener/fetcher/GeoLocationFetcherService.java
+++ src/main/java/com/crazyLabz/shortener/fetcher/GeoLocationFetcherService.java
@@ -2,6 +2,8 @@ package com.crazyLabz.shortener.fetcher;
 
 import com.crazyLabz.shortener.entities.GeoLocation;
 import com.google.gson.Gson;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,13 +12,17 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class GeoLocationFetcherService implements GeoLocationFetcher{
 
+    @Setter
+    @Value("${geo.token}")
+    private String token;
+
     private RestTemplate restTemplate = new RestTemplate();
 
-    private static final String DOMAIN = "http://freegeoip.net/json/";
+    private static final String DOMAIN = "http://api.ipstack.com/";
 
     @Override
     public GeoLocation fetchLocation(String clientIp) {
-        ResponseEntity<String> response = restTemplate.exchange(DOMAIN + clientIp, HttpMethod.GET, null, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(DOMAIN + clientIp + "?" + token, HttpMethod.GET, null, String.class);
         return new Gson().fromJson(response.getBody(), GeoLocation.class);
     }
 }
